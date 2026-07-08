4005e722f2d0c0e75d5f575689e66d7843640b25
MisakiDev <thatmisakidev@outlook.com>
2026-07-08 16:31:23 -0400

    build: add tailwindcss support for prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,11 @@
 /** @type {import('prettier').Config} */
 export default {
+  tailwindStylesheet: "./packages/tailwindcss/tailwind.css",
+  plugins: [
+    "prettier-plugin-tailwindcss",
+    "prettier-plugin-classnames",
+    "prettier-plugin-merge",
+  ],
   semi: true,
   tabWidth: 2,
   singleQuote: false,
